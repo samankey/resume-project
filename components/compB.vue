570e70eeef0b3a56b9fd57c0fa22dc84a6915a91
samankey <71953671+samankey@users.noreply.github.com>
2022-09-05 22:50:34 +0900

    Add: 애니메이션 테스트 / transition

---
+++ components/compB.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="aa">
+    테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>테스트<br>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.aa {
+  background-color: yellow;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,91 @@
 <template>
   <div>
-    테스트
+    <div class="flex">
+      <div class="first text" @click="bb">
+        테스트
+      </div>
+      <div class="second text" @click="() => isShowShow = !isShowShow">
+        테스트
+      </div>
+    </div>
+    <transition
+      name="accordion-item"
+      @enter="startTransition"
+      @after-enter="endTransition"
+      @before-leave="startTransition"
+      @after-leave="endTransition"
+    >
+      <CompA v-if="isShow" />
+    </transition>
+    <transition
+      name="accordion-item"
+      @enter="startTransition"
+      @after-enter="endTransition"
+      @before-leave="startTransition"
+      @after-leave="endTransition"
+    >
+      <CompB v-if="isShowShow" />
+    </transition>
+    <div class="third text">안녕</div>
   </div>
 </template>
 
 <script>
+import CompA from '@/components/compA.vue'
+import CompB from '@/components/compB.vue'
 export default {
-  name: 'IndexPage',
+  components: {
+    CompA, CompB
+  },
+
+  data () {
+    return {
+      isShow: false,
+      isShowShow: false
+    }
+  },
+
+  methods: {
+    bb () {
+      this.isShow = !this.isShow;
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  width: 100%;
+
+}
+.first {
+  background: wheat;
+  flex: 1;
+}
+
+.second {
+  background: rgb(17, 138, 194);
+  flex: 1;
+}
+
+.third {
+  background: rgb(173, 17, 194);
+  flex: 1;
+}
+
+.text {
+  padding: 1.5rem;
+  font-size: 8rem;
+}
+.accordion-item-enter-active, .accordion-item-leave-active {
+  will-change: all;
+  transition: all 0.2s ease;
+}
+.accordion-item-enter, .accordion-item-leave-to {
+  height: 0 !important;
+  padding: 0 !important;
+  margin: 0 !important;
+  opacity: 0 !important;
+}
+</style>
